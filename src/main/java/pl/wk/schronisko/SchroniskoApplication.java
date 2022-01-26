baae032407e059d6445df3b1a5a8608f1df10010
Wiktoria Kulczyńska <wiktoriakulczynska@gmail.com>
2022-01-26 16:38:13 +0100

    Stworzenie projektu

---
+++ src/main/java/pl/wk/schronisko/SchroniskoApplication.java
@@ -0,0 +1,13 @@
+package pl.wk.schronisko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchroniskoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchroniskoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wk/schronisko/SchroniskoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wk.schronisko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchroniskoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
